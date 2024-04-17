cfd3e35e63bd0f41b65ceccf6ae0fbe414462325
Athena Sy Kuzuhara <sykuzuha@usc.edu>
2024-04-17 00:01:45 +0000

    finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool canScheduleWorker(int day, Worker_T worker, const AvailabilityMatrix& avail, const std::vector<int>& shifts, size_t maxShifts);
+bool scheduleHelper(int day, int slot, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, std::vector<int>& shifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +37,46 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
-
-
-
+    int numDays = avail.size(); 
+    int numWorkers= avail[0].size();
+    std::vector<int> shifts(numWorkers, 0);
+    
+    sched = DailySchedule(numDays, std::vector<Worker_T>(dailyNeed));
+    
+    return scheduleHelper(0, 0, avail, dailyNeed, maxShifts, sched, shifts);
+    
 }
+
+bool canScheduleWorker(int day, Worker_T worker, const AvailabilityMatrix& avail, const std::vector<int>& shifts, size_t maxShifts) {
+    return avail[day][worker] && shifts[worker] < maxShifts;
+}
+
+bool scheduleHelper(int day, int slot, const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, std::vector<int>& shifts) {
+    int numDays = avail.size(); 
+    int numWorkers = avail[0].size(); 
+    
+    if (day == numDays) { 
+        return true;
+    }
+    
+    if (slot == dailyNeed) { 
+        return scheduleHelper(day + 1, 0, avail, dailyNeed, maxShifts, sched, shifts);
+    }
+    
+    for (Worker_T worker = 0; worker < numWorkers; ++worker) {
+        if (canScheduleWorker(day, worker, avail, shifts, maxShifts)) {
+            sched[day][slot] = worker;
+            shifts[worker]++;
+            
+            if (scheduleHelper(day, slot + 1, avail, dailyNeed, maxShifts, sched, shifts)) {
+                return true;
+            }
+            shifts[worker]--;
+        }
+    }
+    
+    return false;
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -1,3 +1,4 @@
+
 #ifndef RECCHECK
 // For debugging
 #include <iostream>
@@ -5,6 +6,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +15,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool isValid(std::string word, const std::set<std::string>& dict);
+void makeWords(int letter, const std::string& floating, std::string& word, std::set<std::string>& words, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +25,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::string word = in;
+    std::set<std::string> words;
+    makeWords(0, floating, word, words, dict);
+    
+    
+    return words;
+}
+bool isValid(std::string word, const std::set<std::string>& dict )
+{
+    std::set<std::string>::iterator it = dict.find(word);
+    if (it != dict.end()) // we found the element
+    {
+        return true;
+    }
+    return false;
 }
 
-// Define any helper functions here
+void makeWords(int letter, const std::string& floating, std::string& word, std::set<std::string>& words, const std::set<std::string>& dict)
+{
+    if((letter == word.size())) //Base case
+    {
+        if(isValid(word, dict))
+        {
+            words.insert(word);
+        }
+        return;
+    }
+
+    if(word[letter] != '-')
+    {
+        makeWords(letter + 1, floating, word, words, dict);
+        return;
+    }
+    int count = 0;
+    for (int i = 0; i < word.size(); i++) {
+        if (word[i] == '-') {
+            count++;
+        }
+    }
+    for(int i = 0; i < floating.size(); i++)
+    {
+        word[letter] = floating[i];
+        makeWords(letter + 1, floating.substr(0, i) + floating.substr(i + 1), word, words, dict);
+    }
+    if(count != floating.size())
+    {
+        for(char i = 'a'; i <= 'z'; ++i)
+        {
+            word[letter] = i;
+            makeWords(letter + 1, floating, word, words, dict);
+        }
+    }
+    word[letter] = '-';
+    return;
+}
+ 
